6db55e26279e0cc76fbff7e2e4d46d66346e36f5
Maddy Haldeman <madelyn.haldeman@gmail.com>
2019-03-29 15:17:20 -0700

    change user var to product list and the context to 'products'

--- Projects/techproject/src/mainapp/mainapp/views.py
+++ Projects/techproject/src/mainapp/mainapp/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-  user = request.user
+  products = ["Cherries", "Apples", "Oranges", "Strawberries", "Pears", "Watermelons"]
   context = {
-    'user': user,
+    'products': products,
   }
   return render(request, "home.html", context)
