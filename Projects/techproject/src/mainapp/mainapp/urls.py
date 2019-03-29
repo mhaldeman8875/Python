25839bff7770754d5ae78227f7e802eef65c90f2
Maddy Haldeman <madelyn.haldeman@gmail.com>
2019-03-29 13:09:44 -0700

    add path in .urls

--- Projects/techproject/src/mainapp/mainapp/urls.py
+++ Projects/techproject/src/mainapp/mainapp/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import 
+from .views import path
+
 urlpatterns = [
+    path('^$', views.home, name="home"),
     path('admin/', admin.site.urls),
 ]
