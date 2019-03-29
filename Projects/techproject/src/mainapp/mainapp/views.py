108a805a276a22cd6f29ec36dc718523405f139b
Maddy Haldeman <madelyn.haldeman@gmail.com>
2019-03-29 13:51:20 -0700

    create home.html; link using settings.py templates section

--- Projects/techproject/src/mainapp/mainapp/views.py
+++ Projects/techproject/src/mainapp/mainapp/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-  user = request.user
-  return HttpResponse("<h1>Welcome {}!</h1>".format(user))
+  return render.(request, "home.html", {})
