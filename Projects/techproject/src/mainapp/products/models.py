64f0710ee072c5b7dc28d00e4aa88d9a4ebbdd18
Maddy Haldeman <madelyn.haldeman@gmail.com>
2019-03-28 17:57:18 -0700

    create class Product in models.py

--- Projects/techproject/src/mainapp/products/models.py
+++ Projects/techproject/src/mainapp/products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+  type = models.CharField(max_length=60)
+  name = models.models.CharField(max_length=50, default="", blank=True, null=False)
+  description = models.TextField(max_length=100, default="", blank=True)
+  price = models.DecimalField(default=0.00, max_digits=10000, decimal_places=2)
+  image = models.CharField(max_length=255, default="", blank=True,)
