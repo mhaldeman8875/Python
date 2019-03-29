1edd071f8e76da4668dd25efbe7108d00675bde8
Maddy Haldeman <madelyn.haldeman@gmail.com>
2019-03-29 12:57:36 -0700

    create choices for type object in product class to make dropdown options

--- Projects/techproject/src/mainapp/products/models.py
+++ Projects/techproject/src/mainapp/products/models.py
@@ -1,10 +1,24 @@
 from django.db import models
 
+# create choices for type object
+TYPE_CHOICES = {
+  ('appetizers','appetizers'),
+  ('entrees','entrees'),
+  ('treats', 'treats'),
+  ('drinks', 'drinks'),
+
+}
+
 # Create your models here.
 class Product(models.Model):
-  type = models.CharField(max_length=60)
+  type = models.CharField(max_length=60, choices=TYPE_CHOICES)
   name = models.CharField(max_length=50, default="", blank=True, null=False)
   description = models.TextField(max_length=100, default="", blank=True)
   price = models.DecimalField(default=0.00, max_digits=10000, decimal_places=2)
   image = models.CharField(max_length=255, default="", blank=True,)
 
+  objects = models.Manager()
+
+  def __str__(self):
+    return self.name
+  
