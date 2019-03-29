477137c907499fc2904fdd5bba22f5dd4dc2fb24
Maddy Haldeman <madelyn.haldeman@gmail.com>
2019-03-29 13:12:04 -0700

    fix bug in urls.py line 17, 21

--- Projects/techproject/src/mainapp/mainapp/urls.py
+++ Projects/techproject/src/mainapp/mainapp/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import path
+from . import views
 
 urlpatterns = [
-    path('^$', views.home, name="home"),
+    path('', views.home, name="home"),
     path('admin/', admin.site.urls),
 ]
